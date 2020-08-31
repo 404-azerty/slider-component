6288632272747b1251d90fa807fc53e861fdd7c8
Johanne <johannemay@icloud.com>
2020-08-31 09:33:00 +0200

    skeletton html

---
+++ main.js
@@ -0,0 +1,15 @@
+const slides = [
+  {
+    image = `./images/image-tanya.jpg`,
+    about = `“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”`,
+    name = `Tanya Sinclair`,
+    role = `UX Engineer`
+  },
+  {
+    image = `./images/image-john.jpg`,
+    about =  `“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”`,
+    name = `John Tarkpor`,
+    role = `Junior Front-end Developer`
+  },
+]
+
